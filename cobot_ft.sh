244545ea7dba924c74472479bd6016023db28d65
Guanning Zeng <zgn21@mails.tsinghua.edu.cn>
2024-04-21 03:29:59 +0800

    recover cobot_ft.sh

--- cobot_ft.sh
+++ cobot_ft.sh
@@ -1,2 +1,2 @@
 python ./scripts/finetune_cobot.py\
-    --name ft_cobot --debug True > ft_cobot2.log
+    --name ft_cobot > /root/autodl-tmp/runs/ft_cobot1.log 2> /root/autodl-tmp/runs/ft_cobot1.err
